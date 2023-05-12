1a3f570d7e8113f277f79caa570372b831666acd
o7t3 <o7t3@fresnou.org>
2023-05-12 14:04:16 -0700

    initial commit

---
+++ Ch3Notes/MadLibs.js
@@ -0,0 +1,28 @@
+const readLine=require('readline-sync')
+const readline = require("readline");
+readLine.question('enter a noun');
+readLine.question('enter a verb');
+readLine.question('enter a plural noun');
+readLine.question('enter a noun');
+readLine.question('enter a adjective');
+readLine.question('enter a verb');
+readLine.question('enter a noun');
+readLine.question('enter a adjective');
+readLine.question('enter a noun');
+readLine.question('enter a verb');
+readLine.question('enter a plural noun');
+readLine.question('enter a plural noun');
+readLine.question('enter a verb');
+readLine.question('enter a noun');
+readLine.question('enter a adjective');
+readLine.question('enter a noun');
+readLine.question('enter a verb');
+readLine.question('enter a plural noun');
+readLine.question('enter a noun');
+
+let mylist=[];
+mylist.pushreadline.question('enter a adjective:');
+mylist.pushreadline.question('enter a plural noun:');
+mylist.pushreadline.question('enter a verb:');
+mylist.pushreadline.question('enter a noun:');
+console.log('my list has ${my-list[,]}and.${my-list[0]}')
