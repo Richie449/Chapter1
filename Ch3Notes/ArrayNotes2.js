1fbac781cd14dd73de67b5c1d182ac333cec5a4d
o7t3 <o7t3@fresnou.org>
2023-05-15 14:30:32 -0700

    initial commit

--- Ch3Notes/ArrayNotes2.js
+++ Ch3Notes/ArrayNotes2.js
@@ -1,11 +1,65 @@
-//arrays are `pass by reference`
-//this means you can accidentally change an array
-//if you change another array that it's equal to
+//1
+function printFirst(arr){
+    console.log(arr[0])
+}
 
-let nums = [5, 4, 3, 2, 1];
-let newNums = nums;
-newNums [0] = 12;
-console.log(`nums:`+ nums);
-console.log(`newNums:`+ newNums);
+printFirst([3,12,7,14]);
+printFirst(['a', 'b', 'c'])
 
-//
+//2
+function printLast(arr){
+    let lastIndex = arr.length-1
+    console.log(arr[lastIndex]);
+}
+
+printLast([3,12,7,14]);
+printLast([3,12,7,14]);
+
+//3
+function upgradedPrintFirst(arr){
+    let arrayLength = arr.length;
+    if (arrayLength == 0){
+        console.log('Empty array')
+    } else{
+        console.log(arr[0])
+    }
+}
+
+upgradedPrintFirst([3,12,7,14]);
+upgradedPrintFirst(['a', 'b', 'c'])
+upgradedPrintFirst([])
+
+function upgradedPrintLast(arr){
+    let arrayLength = arr.length;
+    if (arrayLength == 0){
+        console.log('Empty array')
+    } else{
+        let lastIndex = arr.length-1
+        console.log(arr[lastIndex]);
+    }
+}
+
+upgradedPrintLast([3,12,7,14]);
+upgradedPrintLast(['a', 'b', 'c'])
+upgradedPrintLast([])
+
+//4
+function reverseArray(arr){
+    let lastIndex = arr.length-1;
+    for (let i=lastIndex; i>=0; i--){
+        console.log(arr[i])
+    }
+}
+reverseArray([20, 10, 5, 1]);
+reverseArray(['a', 'b', 'c', 'd', 'e'])
+//5
+function everyThird(arr){
+    for (let i = 2; i<arr.length; i=i+3){
+        console.log(arr[i])
+    }
+}
+
+everyThird([10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130]);
+
+//6
+function everySecond(arr){
