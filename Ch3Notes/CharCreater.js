dab123400ae0e88f190efdd1472464e7fe70daa0
o7t3 <o7t3@fresnou.org>
2023-05-19 13:35:40 -0700

    initial commit

--- Ch3Notes/CharCreater.js
+++ Ch3Notes/CharCreater.js
@@ -1,12 +1,9 @@
 const readLine = require('readline-sync')
 let diceRolled = []
-let character = {
-
+let character1 = {
 //empty to start
-
 }
-
-createChar()
+createChar(character1, diceRolled)
 function rolld6(){
     return Math.ceil(Math.random()*6)
 
@@ -14,15 +11,16 @@ function rolld6(){
 
 function rollDice(amount, list){
     for(let i=0; i<amount; i++){
-        let roll = rolld6();
-        console.log(`You rolled a ${roll}!`);
-        list.push(roll);
+        let roll= rolld6();
+        console.log(`You rolled a ${roll}!`)
+        list.push(roll)
     }
+
 }
 
 function sum(list){
     let sum=0;
-    for (let i=0; i<list.length; i++){
+    for (let i=0; i<list.length; i++) {
         sum += list[i];
     }
     return sum;
@@ -35,8 +33,7 @@ function rollStat(list){
 
 }
 
-function createChar(){
-
+function createChar(character, list){
     character.name = readLine.question("Enter Character Name: ");
     console.log('Rolling STR')
     character.strength= rollStat(diceRolled);
@@ -48,8 +45,44 @@ function createChar(){
     character.intelligence= rollStat(diceRolled);
     console.log('Rolling WIS')
     character.wisdom= rollStat(diceRolled);
-    console.log('Rolling CHA')
-    character.charisma= rollStat(diceRolled);
-    console.log(character)
+    console.log('Rolling PWR')
+    character.power= rollStat(diceRolled);
+    console.log('Rolling SPD')
+    character.speed= rollStat(diceRolled);
+    console.log('Rolling WPN')
+    character.weaponmastery= rollStat(diceRolled);
+
+    DisplayChar(character)
+
+}
+
+function DisplayChar(char){
+
+
+
+
+
+
+
+
+
+
+    console.log(`Name: ${char.name}`);
+
+    console.log(`STR: ${char.strength}`);
+
+    console.log(`DEX: ${char.dexterity}`);
+
+    console.log(`CON: ${char.constitution}`);
+
+    console.log(`INT: ${char.intelligence}`);
+
+    console.log(`WIS: ${char.wisdom}`);
+
+    console.log(`PWR: ${char.power}`);
+
+    console.log(`SPD: ${char.speed}`);
+
+    console.log(`WPN: ${char.weaponmastery}`);
 
 }
